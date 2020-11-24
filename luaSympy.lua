9f5f42bc4573444ea78feecabee266a72affe48e
Aaron English <humdrumcomet@protonmail.com>
2020-11-24 00:01:21 -0500

    Working in the base form. must add allowances for sympy functions with equations and vars used

--- luaSympy.lua
+++ luaSympy.lua
@@ -1,1 +1,30 @@
-function varEqtoGls()
+function pgDictToTable(importLoc)
+eqTab = {}
+varTab = {}
+python.execute([[var, eqt = lsh.getVandEfromFile(']]..importLoc..[[')
+for key, value in var:
+    lua.eval('eqTab['+key+']='+value)
+for key, value in var:
+    lua.eval('varTab['+key+']='+value)
+]])
+    print(type(eqTab))
+    print(eqTab)
+    for key, value in pairs(eqTab) do
+        print(key, value)
+    end
+    return {varTab, eqTab}
+end
+
+function optsToTable(opts)
+    print(opts)
+    if opts == 'path' then
+        return ''
+    end
+    inputs = {}
+    for key, value in string.gmatch(opts, '(%w+)%s*=%s*(.*),*') do
+        print(key)
+        print(value)
+        inputs[key] = value
+    end
+    return inputs
+end
